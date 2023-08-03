9fa641aba4e8ae9a2bba917679a8dbb7eb4b5d8f
Luana050 <127446320+Luana050@users.noreply.github.com>
2023-08-03 16:11:00 -0400

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,43 +7,122 @@ void main() {
 }
 
 class PaginaInical extends StatelessWidget {
-PaginaInical({super.key});
+  PaginaInical({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      routes: {
+        '/': (context) => Home(),
+        '/contador': (context) => Contador(),
+        '/curtir': (context) => Curtir(),
+      },
+    );
+  }
+}
+
+class Curtir extends StatelessWidget {
+  const Curtir({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Curtir"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            IconButton(
+              iconSize: 50,
+              icon: Icon(Icons.favorite_outline),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
 class Home extends StatelessWidget {
-Home({super.key});
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    String x = "100";
-
     return Scaffold(
       appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () {
+              Navigator.pushNamed(context, '/contador');
+            },
+            leading: Icon(Icons.calculate,
+                size: 36, color: Color.fromARGB(255, 110, 194, 50)),
+            title: Text("Contador"),
+            subtitle: Text("Exemplo de incremento e decremento"),
+            trailing: Icon(Icons.chevron_right),
+          ),
+          ListTile(
+            onTap: () {
+              Navigator.pushNamed(context, '/curtir');
+            },
+            leading: Icon(Icons.favorite,
+                size: 36, color: Color.fromARGB(255, 233, 53, 40)),
+            title: Text("Curtir"),
+            subtitle: Text("Exemplo de Curtir e Descurtir"),
+            trailing: Icon(Icons.chevron_right),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Contador extends StatefulWidget {
+  const Contador({super.key});
+
+  @override
+  State<Contador> createState() => _ContadorState();
+}
+
+class _ContadorState extends State<Contador> {
+  int x = 100;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 142, 46, 159),
         title: Text("Meu Aplicativo"),
       ),
       body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(x),
-          ElevatedButton(
-            onPressed: () {
-              // AO PRESSIONAR O BOTAO
-              print("Antes de Zerar");
-              print(x);
-              x = "0";
-              print("Depois de Zerar");
-              print(x);
-            },
-            child: Text("Zerar"),
-          
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(x.toString()),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 142, 46, 159)),
+              onPressed: () {
+                setState(() {
+                  x = x + 1;
+                });
+              },
+              child: Text("Incrementa"),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 142, 46, 159)),
+              onPressed: () {
+                setState(() {
+                  x = x - 1;
+                });
+              },
+              child: Text("Decrementa"),
             )
           ],
         ),
